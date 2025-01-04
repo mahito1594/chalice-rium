f964098bca3de337a316b8e08b016ec021648d21
Mahito TANNO <mahito@presche.me>
2025-01-04 18:13:12 +0900

    feat(WIP):  create User profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find_by(username: params[:username])
+    if @user.nil?
+      # Todo: should return 404 not found
+      redirect_to root_path
+    end
   end
 
   def edit
