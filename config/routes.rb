7b5782fdd527c03f8d882af094df827fcbc746d8
Mahito TANNO <mahito@presche.me>
2025-01-12 21:15:40 +0900

    chore:  delete unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,7 @@ Rails.application.routes.draw do
   root to: "static_pages#home"
 
   # Custom routes for users
-  resources :users, param: :username, only: %i[show edit update destroy] do
-    member do
-      get :dungeons
-    end
-  end
+  resources :users, param: :username, only: %i[show edit update destroy]
 
   # Todo: users の member として扱う
   resources :dungeons
