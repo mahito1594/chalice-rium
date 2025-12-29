6446f681d0237f1460042fc6723e4bcf5cb0bdad
Mahito TANNO <mahito@presche.me>
2025-12-29 22:02:06 +0900

    chore: replace old style syntax in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up", to: "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -14,8 +14,8 @@ Rails.application.routes.draw do
   root to: "dungeons#index"
 
   # Routes for the static resources
-  get "about" => "static_pages#about", as: :about
-  get "terms" => "static_pages#terms", as: :terms
+  get "about", to: "static_pages#about", as: :about
+  get "terms", to: "static_pages#terms", as: :terms
 
   # Custom routes for users
   resources :users, param: :username, only: %i[show edit update destroy]
